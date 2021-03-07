d6caba21c1488dd10675566ea24d52b0f2f31b8f
Rachman Esa <esareynor99@gmail.com>
2021-03-07 20:11:31 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -23,22 +23,22 @@ Route::post('candidate/register', 'API\UserController@register');
 
 //User Profile route ================================================================================ ( WITH BEARER TOKENS AUTHORIZATIONS )
 Route::prefix('user')->name('candidate.setting.')->middleware('auth:api')->group(function () {
-  Route::get('/profile','API\UserController@details'); // User Detail
-  Route::put('/profile/update','API\UserController@updateProfile'); // Update Profile
-  Route::post('/cv/upload/{user_id}','API\ContentController@cv_uploader'); //upload Resume
+  Route::get('/profile', 'API\UserController@details'); // User Detail
+  Route::put('/profile/update', 'API\UserController@updateProfile'); // Update Profile
+  Route::post('/cv/upload/{user_id}', 'API\ContentController@cv_uploader'); //upload Resume
 });
 
 // Front main route
-Route::prefix('front-candidate')->name('c.front.')->middleware('auth:api')->group(function(){
-  Route::post('/search','API\FrontController@JobbySearch');
+Route::prefix('front-candidate')->name('c.front.')->middleware('auth:api')->group(function () {
+  Route::post('/search', 'API\FrontController@JobbySearch');
 });
 
 // Job activity
-Route::prefix('candidate-job')->name('c.job.')->middleware('auth:api')->group(function(){
-  Route::post('/apply-job','API\JobController@apply')->name('apply');
-  Route::post('/add-favorite','API\JobController@favoritingJob')->name('add_fav');
+Route::prefix('candidate-job')->name('c.job.')->middleware('auth:api')->group(function () {
+  Route::post('/apply-job', 'API\JobController@apply')->name('apply');
+  Route::post('/add-favorite', 'API\JobController@favoritingJob')->name('add_fav');
 });
 
 // Image
-Route::get('/user/image','API\ContentController@userImage')->name('user.image');
-Route::get('/company/image/{id_company}','API\ContentController@companyImage')->name('company.image');
+Route::get('/user/image', 'API\ContentController@userImage')->name('user.image');
+Route::get('/company/image/{id_company}', 'API\ContentController@companyImage')->name('company.image');
